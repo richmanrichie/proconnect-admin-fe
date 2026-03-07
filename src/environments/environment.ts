d72ec0c1ea84dc98814abb333a2cb209e75c423c
richard.ali <richardali@ymail.com>
2026-03-07 14:27:27 +0100

    Update environment configuration for production settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiBaseUrl: '/api/v1',
-  merchantCode: 'MX007',
-  payItemId: '101007',
+  production: true,
+  apiBaseUrl: 'https://proconnect-lingering-rain-3381.fly.dev/api/v1',
+  merchantCode: 'MX007', // Replace with production merchant code
+  payItemId: '101007', // Replace with production pay item ID
   payment: {
     scriptUrl: 'https://newwebpay.qa.interswitchng.com/inline-checkout.js',
     currency: 566, // NGN
     mode: 'TEST'
   }
-};
-
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
